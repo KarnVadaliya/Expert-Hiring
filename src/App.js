d08edb67f7f6c6fbfe6be68657ad2ca8f8e666a8
Karn Vadaliya <vadaliya.k@husky.neu.edu>
2020-03-23 14:15:42 -0400

    Added by Karn

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Hello, Git Successfully Added from command line</h1>
+        <h1> Hello, Git Successfully Added from Karn</h1>
       </header>
     </div>
   );
