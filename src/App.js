31fdad88e78687af02c72b9f7a7581cd7240af89
Priyam Suthar <suthar.p@husky.neu.edu>
2020-03-23 14:44:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Hello, Git Successfully Added from Karn from Command line </h1>
+        <h1> Hello, Git Successfully Added from Karn from Command lin. Hello kaun</h1>
       </header>
     </div>
   );
