cb326fc3d2dd3ec6eb40bce6d222e0632748c4ba
Deep Gamit <gamit.d@husky.neu.edu>
2020-03-23 13:22:19 -0400

    git success

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Hello, React</h1>
+        <h1> Hello, Git Successfully Added</h1>
       </header>
     </div>
   );
