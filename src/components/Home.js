c6419c4884c8eeaca148a41b746f265afc7b83b3
Priyam Suthar <suthar.p@husky.neu.edu>
2020-04-05 21:04:27 -0400

    npm run ganesh

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import Searchbar from './Searchbar';
 import ServiceBar from './ServiceBarComponent';
 import Footer from './Footer';
+import Services from './ServicesComponent';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
                 <Searchbar></Searchbar>
-                {/* {console.log(this.state.service)} */}
                 <ServiceBar></ServiceBar>
+                <Services></Services>
                 <Footer/>
             </div>
         )
