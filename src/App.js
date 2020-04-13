5c6b58a60f0cef3ee2f60d892bd76ca023af8bc6
Deep Gamit <gamit.d@husky.neu.edu>
2020-04-13 14:53:09 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-// import ServiceBar from './components/ServiceBarComponent';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
@@ -9,12 +8,8 @@ import Footer from './components/Footer';
 import Careers from './components/Careers';
 import SofaCleaning from './components/Cleaning/SofaCleaning';
 import Example from './components/Example';
-import MakeUp from './components/Services/Makeup';
 import SalonAtHome from './components/Services/SalonAtHome';
-
-
-
-import Makeup from './components/Makeup';
+import Makeup from './components/Services/Makeup';
 import Payment from './components/Payment';
 
 function App() {
@@ -25,7 +20,7 @@ function App() {
           <Route path="/" exact component={Home}></Route>  
           <Route path="/about" component={AboutUs}></Route>  
           <Route path="/contact" component={ContactUs}></Route> 
-          <Route path="/makeup" component={MakeUp}></Route> 
+          <Route path="/makeup" component={Makeup}></Route> 
           <Route path="/payment" component={Payment}></Route> 
           <Route path="/salon-at-home" component={SalonAtHome}></Route> 
           <Route path="/cleaning/sofaCleaning" component={SofaCleaning}></Route>
